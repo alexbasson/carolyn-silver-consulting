fd873b3b348e4fc891855d87bd99fd959236e634
Alex Basson <alexbasson@gmail.com>
2020-01-07 11:11:01 -0500

    Convert styles to sass

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import {Navigation} from './Navigation';
 import '../styles/Header.scss';
 
 export const Header = () => (
-  <header className="Header">
+  <header className="header">
     <h1>Carolyn Silver Consulting</h1>
     <Navigation/>
   </header>
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 import '../styles/Navigation.scss';
 
 export const Navigation = () => (
-  <nav className="Navigation">
+  <nav className="nav">
     <div className="link"><Link to="/">Home</Link></div>
     <div className="link"><Link to="/about">About</Link></div>
     <div className="link"><Link to="/services">Services</Link></div>
