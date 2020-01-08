1f510fd3720b777ef00669d49b68b92a924b564a
Alex Basson <alexbasson@gmail.com>
2020-01-07 22:14:09 -0500

    Auto generate grid classes

--- src/components/ContentPage.tsx
+++ src/components/ContentPage.tsx
@@ -17,11 +17,11 @@ export const ContentPage: React.FC<Props> = ({title, image, children}) => {
         <h1 className="title">{title}</h1>
         <div className="grid">
           <div className="grid-inner">
-            <div className="col-4-desktop col-2-tablet col-4-mobile img">
+            <div className="col-4-desktop col-2-tablet col-4-phone img">
               <img src={image} alt={title}/>
             </div>
 
-            <div className="col-8-desktop col-6-tablet col-4-mobile text">
+            <div className="col-8-desktop col-6-tablet col-4-phone text">
               {children}
             </div>
           </div>
